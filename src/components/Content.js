60a756a69c714caa32548ddf1b6d721996bf3e61
Dmitry Rukovichnikov <dmitry.rukovichnikov@gmail.com>
2021-05-10 19:30:02 +0300

    init: initial commit

---
+++ src/components/Content.js
@@ -0,0 +1,11 @@
+function Content (){
+    return (
+        <div className="content">
+            <div className="main_sidebar__left">LEFT SIDEBAR</div>
+            <div className="main_content">CONTENT</div>
+            <div className="main_sidebar__right">RIGHT SIDEBAR</div>
+        </div>
+    )
+}
+
+export default Content
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+function Header(){
+    return (
+        <div className="header">
+            <div className="header_logo">LOGO</div>
+            <div className="header_search-field">SEARCH FIELD</div>
+            <div className="header_drop-down">DROP DOWN</div>
+            <div className="header_burger-menu">BURGER MENU</div>
+        </div>
+    )
+}
+
+export default Header
